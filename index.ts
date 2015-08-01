3ddefb04867bfc11e20db99bd681058202122dc5
tkQubo <tk.qubo@gmail.com>
2015-08-01 13:51:44 +0900

    Fix bug

--- index.ts
+++ index.ts
@@ -1,5 +1,4 @@
 'use strict';
-import config from './config';
 import * as moment from 'moment';
 import * as rx from 'rx';
 import * as _ from 'lodash';
@@ -20,26 +19,28 @@ let repoInfo = {
     repo: argv.repo
 };
 
-github.issues.getAllMilestones(repoInfo, (error: any, milestones: any) => {
+github.issues.getAllMilestones(repoInfo, (error: any, milestones: any[]) => {
     if (!!error) {
         console.log('Failed to get milestones');
         console.error(error);
         return;
     }
+    if (argv.verbose) {
+        console.log('Milestones:');
+        console.dir(milestones);
+    }
 
-    let createMilestone: (() => Promise<any>) =
-        argv.dryRun ? Promise.resolve.bind(null, true) : Promise.promisify(github.issues.createMilestone);
     let template = _.template(argv.format);
     let startDate: moment.Moment = moment(argv.date);
     rx.Observable
         .range(argv.start, argv.count)
         .map((index) => { // Construct post body
-            let m = startDate.add(argv.period * index - 1);
-            var date = m.format('YYYY-MM-DD');
+            let m = startDate.add(argv.period * index);
+            var date = startDate.format('YYYY-MM-DD');
             let w = Math.ceil(m.date() / 7);
 
             let title = template({ index, date, w });
-            let due_on = m.add(argv.period, 'd').toISOString();
+            let due_on = m.add(argv.period - 1, 'd').toISOString();
 
             let body = {title, due_on};
             _.merge(body, repoInfo);
@@ -47,22 +48,33 @@ github.issues.getAllMilestones(repoInfo, (error: any, milestones: any) => {
             return body;
         })
         .map((body: any) => { // Combine post body and the existence of duplicated milestone
-            let exists = !_.where(milestones, { title: body.title }).length;
+            let exists = !!_.where(milestones, { title: body.title }).length;
             if (argv.verbose) {
                 console.log(`request construct: ${JSON.stringify({ body, exists }, null, 2)}`);
             }
             return { body, exists };
         })
         .filter((paramsWithExistence) => !paramsWithExistence.exists)
-        .pluck('params')
-        .flatMap(createMilestone)
-        .subscribe((response) => {
+        .pluck('body')
+        .flatMap((body: any) => {
+            if (argv.dryRun) {
+                return Promise.resolve(true);
+            }
+
+            return new Promise((resolve: Function, reject: Function) => {
+                github.issues.createMilestone(body, (response: any) => {
+                   resolve(response);
+                });
+            });
+        })
+        .subscribe((response: any) => {
             if (argv.verbose) {
                 console.log(`Create result: ${response}`);
             }
-        }
-        , console.error
-        , () => {
+        }, (error: any) => {
+            console.log('error');
+            console.error(error);
+        }, () => {
             if (argv.verbose) {
                 console.log('Finished');
             }
