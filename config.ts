10b6c679847094db1bfc793f07b629bd5cdd806e
tkQubo <tk.qubo@gmail.com>
2015-08-01 12:49:56 +0900

    Implement core logic

--- config.ts
+++
@@ -1,6 +0,0 @@
-'use strict';
-let config = {
-    githubToken: process.env.GMG_GITHUB_TOKEN
-};
-
-export default config;
--- index.ts
+++ index.ts
@@ -1,39 +1,72 @@
 'use strict';
 import config from './config';
 import * as moment from 'moment';
+import * as rx from 'rx';
+import * as _ from 'lodash';
+import * as Promise from 'bluebird';
+import argv from './argv';
 const GitHubApi: any = require('node-github');
 const github: any = new GitHubApi({
     version: "3.0.0",
-    debug: true
+    debug: argv.verbose
 });
-const argv = require('yargs')
-        .usage('Usage: $0 -u [user] -r [repo] -d [date]')
-        .demand(['u', 'r', 'd'])
-        .alias('u', 'user')
-        .alias('r', 'repo')
-        .alias('d', 'date')
-        .describe('u', 'User or organization name')
-        .describe('r', 'Repository to create milestone')
-        .describe('d', 'Date from which the milestone starts (yyyy-MM-dd)')
-        .epilog('copyright 2015')
-        .argv
-    ;
-
-
-let now = moment().toISOString();
 
 github.authenticate({
     type: "oauth",
-    token: config.githubToken
+    token: argv.githubToken || process.env.GMG_GITHUB_TOKEN
 });
-
-github.issues.getAllMilestones({
+let repoInfo = {
     user: argv.user,
     repo: argv.repo
-}, (error: any, response: any) => {
+};
+
+github.issues.getAllMilestones(repoInfo, (error: any, milestones: any) => {
     if (!!error) {
-        console.error(error)
+        console.log('Failed to get milestones');
+        console.error(error);
+        return;
     }
-    console.dir(response);
+
+    let createMilestone: (() => Promise<any>) =
+        argv.dryRun ? Promise.resolve.bind(null, true) : Promise.promisify(github.issues.createMilestone);
+    let template = _.template(argv.format);
+    let startDate: moment.Moment = moment(argv.date);
+    rx.Observable
+        .range(argv.start, argv.count)
+        .map((index) => { // Construct post body
+            let m = startDate.add(argv.period * index - 1);
+            var date = m.format('YYYY-MM-DD');
+            let w = Math.ceil(m.date() / 7);
+
+            let title = template({ index, date, w });
+            let due_on = m.add(argv.period, 'd').toISOString();
+
+            let body = {title, due_on};
+            _.merge(body, repoInfo);
+
+            return body;
+        })
+        .map((body: any) => { // Combine post body and the existence of duplicated milestone
+            let exists = !_.where(milestones, { title: body.title }).length;
+            if (argv.verbose) {
+                console.log(`request construct: ${JSON.stringify({ body, exists }, null, 2)}`);
+            }
+            return { body, exists };
+        })
+        .filter((paramsWithExistence) => !paramsWithExistence.exists)
+        .pluck('params')
+        .flatMap(createMilestone)
+        .subscribe((response) => {
+            if (argv.verbose) {
+                console.log(`Create result: ${response}`);
+            }
+        }
+        , console.error
+        , () => {
+            if (argv.verbose) {
+                console.log('Finished');
+            }
+        })
+    ;
 });
 
